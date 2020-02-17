a3f85b58d8493fa9e59262a91a0e17a899cb1188
Arnaud Schaeffer <aschaeffer.eu@gmail.com>
2020-02-17 22:46:23 +0100

    Resource is busy

--- rootfs/apache-template.sh
+++ rootfs/apache-template.sh
@@ -7,4 +7,6 @@ containerName=${CONTAINER_NAME:-symfony_php}
 
 set -x
 
-sed -i -e "s/{SERVER_NAME}/$serverName/g" -e "s/{SERVER_ALIAS}/$serverAlias/g" -e "s/{CONTAINER_NAME}/$containerName/g" /etc/apache2/sites-enabled/symfony.conf
+cp /etc/apache2/sites-enabled/symfony.conf ~/symfony.conf
+sed -i -e "s/{SERVER_NAME}/$serverName/g" -e "s/{SERVER_ALIAS}/$serverAlias/g" -e "s/{CONTAINER_NAME}/$containerName/g" ~/symfony.conf
+cp -f ~/symfony.conf /etc/apache2/sites-enabled/symfony.conf
