fb6fd1ba787e111ab6010224cfdfb0d17270a009
Arnaud Schaeffer <aschaeffer.eu@gmail.com>
2020-02-18 22:30:10 +0100

    Update sed script

--- rootfs/apache-template.sh
+++ rootfs/apache-template.sh
@@ -7,6 +7,6 @@ containerName=${CONTAINER_NAME:-symfony_php}
 
 set -x
 
-cp /etc/apache2/sites-enabled/symfony.conf ~/symfony.conf
-sed -i -e "s/{SERVER_NAME}/$serverName/g" -e "s/{SERVER_ALIAS}/$serverAlias/g" -e "s/{CONTAINER_NAME}/$containerName/g" ~/symfony.conf
-cp -f ~/symfony.conf /etc/apache2/sites-enabled/symfony.conf
+cp /etc/apache2/sites-enabled/symfony.conf /usr/local/apache2/symfony.conf
+sed -i -e "s/{SERVER_NAME}/$serverName/g" -e "s/{SERVER_ALIAS}/$serverAlias/g" -e "s/{CONTAINER_NAME}/$containerName/g" /usr/local/apache2/symfony.conf
+cp -f /usr/local/apache2/symfony.conf /etc/apache2/sites-enabled/symfony.conf
